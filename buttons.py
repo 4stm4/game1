0504f9cdd92d2eacef89882f28244876d0791b82
Aleksei Zakharchenko <i@4stm4.ru>
2019-11-09 17:33:24 +0500

    mll

--- buttons.py
+++ buttons.py
@@ -10,8 +10,7 @@ butttons = []
 class BUTTON(object):
     def __init__(self, number, pin_sensor, pin_led, points : int, start : bool):
         self.number = number
-        self.sensor = gpiozero.Button(pin_sensor)
-        self.sensor.pull_up = False
+        self.sensor = gpiozero.Button(pin_sensor, pull_up = False, bounce_time= None)
         self.led = gpiozero.LED(pin_led)
         self.points_per_click = points
         self.start = start

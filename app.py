b8b558915f7ca97ed5ce852f7d4343dca4c4a580
Aleksei Zakharchenko <i@4stm4.ru>
2019-11-10 13:34:54 +0500

    jgtud89

--- app.py
+++ app.py
@@ -73,8 +73,7 @@ def start_button_work():
             if cnt<1:
                 cnt += 1
                 print('start {}'.format(cnt))
-                with app.app_context():
-                    return redirect(url_for('start'))
+                return redirect('http://127.0.0.1/start')
                 break
 
 def buttons_work(): 

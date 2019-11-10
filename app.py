6a5a88a047135074a4a6eb4b1e87a63ec162e2a1
Aleksei Zakharchenko <i@4stm4.ru>
2019-11-10 13:14:27 +0500

    m.jhkjgio

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def start_button_work():
             if cnt<1:
                 cnt += 1
                 print('start {}'.format(cnt))
-                return redirect('http://127.0.0.1/start', code=302)
+                return redirect(url_for('start'))
                 break
 
 def buttons_work(): 

07835e98afc0b52579abb130b3732ca1a9036ea0
Aleksei Zakharchenko <i@4stm4.ru>
2019-11-10 11:09:19 +0500

    lnljn

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import cv2, time, os, pysnooper, datetime, pygame
 from pygame import camera
 from db import SQL
-from utils import do_photo, play_music
+from pygame import mixer
 from flask import Flask, render_template, send_from_directory
 from buttons import butttons, start_button, buttons_specs, BUTTON
 from threading import Thread
@@ -37,6 +37,21 @@ def index():
             }
         )
     return render_template('history.html', rating = history_list)
+
+def do_photo(name, path):
+    cap = cv2.VideoCapture(0) # Включаем первую камеру
+    for i in range(30): cap.read() # "Прогреваем" камеру, чтобы снимок не был тёмным 
+    ret, frame = cap.read() # Делаем снимок 
+    #frame = frame[300, 150] обрезать фото
+    photo_file = '{}/{}'.format(os.path.join(path, 'static/photo'),name)
+    cv2.imwrite(photo_file, frame) # Записываем в файл
+    cap.release() # Отключаем камеру
+    return ''
+
+def play_music(mp3_file:str):
+    mixer.init()
+    mixer.music.load(mp3_file)
+    mixer.music.play()
 
 @app.route('/start')
 def start_game():

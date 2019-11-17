64d8cbaa4d13792e12278974de8a354399d650ab
Aleksei Zakharchenko <i@4stm4.ru>
2019-11-17 16:42:10 +0500

    kbjg90

--- app.py
+++ app.py
@@ -118,10 +118,7 @@ def start_button_work():
                 continue
 
 def but_music_blink(but_num:int):
-        play_music('static/music/button.mp3')
-        butttons[but_num].led.off()
-        time.sleep(3)
-        butttons[but_num].led.on()
+
 
 def buttons_work(): 
     global game_points, game_phase
@@ -130,8 +127,10 @@ def buttons_work():
         if game_phase == 0:
             for i in butttons:
                 if i.sensor.is_active:
-                    but_action = Thread(target = but_music_blink, args = (i.number,))
-                    but_action.start()
+                    play_music('static/music/button.mp3')
+                    butttons[i.number].led.off()
+                    time.sleep(3)
+                    butttons[i.number].led.on()
                     continue
         else:
             if game_phase ==2:

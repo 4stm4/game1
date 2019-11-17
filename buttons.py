7ad326d007c2f893a8a60a4777cd06955422fed0
Aleksei Zakharchenko <i@4stm4.ru>
2019-11-17 12:26:57 +0500

    hjgf

--- buttons.py
+++ buttons.py
@@ -2,18 +2,18 @@ import gpiozero, pysnooper, time, requests
 from utils import do_photo, play_music
 from threading import Thread
 
-
+# pin_sensor, pin_led, points : int, start
 buttons_specs = [
-    [20, 16, 20, False]
+    [20, 16, 20],
+    [7, 8, 15]
 ]
 butttons = []
 
 class BUTTON(object):
-    def __init__(self, number, pin_sensor, pin_led, points : int, start : bool):
+    def __init__(self, number, pin_sensor, pin_led, points : int):
         self.number = number
         self.sensor = gpiozero.Button(pin_sensor, pull_up = False, bounce_time= None)
         self.led = gpiozero.LED(pin_led)
         self.points_per_click = points
-        self.start = start
 
-start_button = BUTTON(0, 21, 12, 0, True)
+start_button = BUTTON(0, 21, 12, 0)

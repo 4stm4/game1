d7a209d797fb02a15b81ef54390847ce6ebbd557
Aleksei Zakharchenko <i@4stm4.ru>
2019-12-15 17:42:16 +0500

    add changes

--- app.py
+++ app.py
@@ -1,54 +1,70 @@
 """ Игра для батутной арены"""
-import cv2, time, os, datetime, pygame, random, pysnooper
-from db import SQL
-from pygame import mixer
-from flask import Flask, render_template, send_from_directory, redirect, url_for
-from buttons import butttons, start_button, buttons_specs, BUTTON
+import time
+import os
+import random
 from threading import Thread
-from webbrowser import open_new
+import cv2
+from pygame import mixer
+from flask import Flask, render_template, send_from_directory
+from db import SQL
+from buttons import butttons, start_button, buttons_specs, BUTTON
 
-app = Flask(__name__)
+
+APP = Flask(__name__)
 #app.config['DEBUG'] = True
-app.config.from_pyfile('config.ini')
-game_phase = 0  # 0 - ожидание, 1 - старт, 2 игра, 3 - результаты
-active_button = -1
-game_points = 0
-gamer_id = -1
+APP.config.from_pyfile('config.ini')
+GAME_PHASE = 0  # 0 - ожидание, 1 - старт, 2 игра, 3 - результаты
+ACTIVE_BUTTON = -1
+GAME_POINTS = 0
+GAMER_ID = -1
 
 
-@app.route('/photo/<filename>')
+@APP.route('/photo/<filename>')
 def photo(filename):
-    return send_from_directory(os.path.join(app.root_path, 'static/photo'),filename, mimetype='image')
+    """Метод выдаёт фотографии из рейтинга
+    """
+    return send_from_directory(os.path.join(APP.root_path, 'static/photo'), filename,
+                               mimetype='image')
 
-@app.route('/music/<filename>')
+@APP.route('/music/<filename>')
 def music(filename):
-    return send_from_directory(os.path.join(app.root_path, 'static/music'),filename, mimetype='audio')
+    """ Метод передаёт файлы звуков
+    """
+    return send_from_directory(os.path.join(APP.root_path, 'static/music'), filename,
+                               mimetype='audio')
 
-@app.route('/game')
+@APP.route('/game')
 def game():
-    global game_phase, game_points
-    game_phase = 2
+    """Фаза игры
+    """
+    global GAME_PHASE, GAME_POINTS
+    GAME_PHASE = 2
     for button in butttons:
         button.led.off()
     start_button.led.off()
-    return render_template('game.html', foto = '/photo/{}.png'.format(gamer_id))
+    return render_template('game.html', foto='/photo/{}.png'.format(GAMER_ID))
 
-@app.route('/game_over')
+@APP.route('/game_over')
 def game_over():
-    global game_phase, game_points, gamer_id
-    game_phase = 3
-    SQL('update', 'update_points',(game_points, gamer_id,))
+    """Фаза завершения игры
+    """
+    global GAME_PHASE, GAME_POINTS, GAMER_ID
+    GAME_PHASE = 3
+    SQL('update', 'update_points', (GAME_POINTS, GAMER_ID,))
     for button in butttons:
         button.led.blink()
     start_button.led.blink()
-    return render_template('game_over.html', points = game_points, foto = '/photo/{}.png'.format(gamer_id))
+    return render_template('game_over.html', points=GAME_POINTS,
+                           foto='/photo/{}.png'.format(GAMER_ID))
 
-@app.route('/')
+@APP.route('/')
 def index():
-    global game_phase, game_points, gamer_id
-    game_phase = 0
-    game_points = 0
-    gamer_id = -1
+    """Фаза ожидания начала игры
+    """
+    global GAME_PHASE, GAME_POINTS, GAMER_ID
+    GAME_PHASE = 0
+    GAME_POINTS = 0
+    GAMER_ID = -1
     for button in butttons:
         button.led.on()
     start_button.led.blink()
@@ -65,73 +81,87 @@ def index():
                 "foto": "/static/photo/{}".format(winner[2])
             }
         )
-    return render_template('history.html', rating = winners_list)
+    return render_template('history.html', rating=winners_list)
 
 def do_photo(name, path):
+    """Метод делает фотографию
+    """
     try:
         camera = cv2.VideoCapture(0) # Включаем первую камеру
-        for i in range(2): camera.read() # "Прогреваем" камеру, чтобы снимок не был тёмным 
-        ret, frame = camera.read() # Делаем снимок 
+        camera.read() # "Прогреваем" камеру, чтобы снимок не был тёмным
+        ret, frame = camera.read() # Делаем снимок
         #frame = frame[300, 150] обрезать фото
         #cv2.ROTATE_90_CLOCKWISE
         #cv2.ROTATE_90_COUNTERCLOCKWISE
         frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-        photo_file = '{}/{}'.format(os.path.join(path, 'static/photo'),name)
+        photo_file = '{}/{}'.format(os.path.join(path, 'static/photo'), name)
         cv2.imwrite(photo_file, frame) # Записываем в файл
         camera.release() # Отключаем камеру
-    except Exception as e:
-        return 'ERROR = {}'.format(e)
+    except Exception as err:
+        return 'ERROR = {}'.format(err)
     return ''
 
-def play_music(mp3_file:str):
+def play_music(mp3_file: str):
+    """Метод проигрывает звуки
+    """
     mixer.init()
     mixer.music.load(mp3_file)
     mixer.music.play()
 
-@app.route('/start')
+@APP.route('/start')
 def start_game():
-    global game_phase, gamer_id
-    if game_phase == 1:
-        game_phase = 2
+    """Фаза начала игры
+    """
+    global GAME_PHASE, GAMER_ID
+    if GAME_PHASE == 1:
+        GAME_PHASE = 2
         for button in butttons:
             button.led.on()
         start_button.led.on()
-        gamer_id = SQL('insert','insert_history')
-        photo_name = '{}.png'.format(gamer_id)
-        do_photo(photo_name, app.root_path)
-        t = Thread(target=play_music, args = ('static/music/start_game.mp3',))
-        t.start()
+        GAMER_ID = SQL('insert', 'insert_history')
+        photo_name = '{}.png'.format(GAMER_ID)
+        do_photo(photo_name, APP.root_path)
+        thread_music = Thread(target=play_music, args = ('static/music/start_game.mp3',))
+        thread_music.start()
 
-        SQL('update', 'update_history',( photo_name, gamer_id,))
-    return render_template('start.html', foto = '/photo/{}.png'.format(gamer_id))
+        SQL('update', 'update_history',( photo_name, GAMER_ID,))
+    return render_template('start.html', foto = '/photo/{}.png'.format(GAMER_ID))
 
-@app.route('/get_game_phase', methods=['POST'])
+@APP.route('/get_game_phase', methods=['POST'])
 def get_game_phase():
-    global game_phase
-    return str(game_phase)
+    """Метод выдаёт фазу игры
+    """
+    global GAME_PHASE
+    return str(GAME_PHASE)
 
-@app.route('/get_game_points', methods=['POST'])
+@APP.route('/get_game_points', methods=['POST'])
 def get_game_points():
-    global game_points
-    return str(game_points)
+    """Метод возвращает количество игровых очков
+    """
+    global GAME_POINTS
+    return str(GAME_POINTS)
 
 
 def start_button_work():
-    global game_phase
+    """Метод работы стартовой кнопки
+    """
+    global GAME_PHASE
     while True:
         time.sleep(0.2)
-        if game_phase == 0:
+        if GAME_PHASE == 0:
             if start_button.sensor.is_active:
-                game_phase = 1
+                GAME_PHASE = 1
                 continue
 
 def buttons_work(): 
-    global game_points, game_phase
+    """Метод работы игровых кнопок
+    """
+    global GAME_POINTS, GAME_PHASE
     start_button_num = len(butttons)
     last_two = []
     while True:
         time.sleep(0.2)
-        if game_phase == 0:
+        if GAME_PHASE == 0:
             for i in butttons:
                 if i.sensor.is_active:
                     play_music('static/music/button.mp3')
@@ -140,7 +170,7 @@ def buttons_work():
                     butttons[i.number].led.on()
                     continue
         else:
-            if game_phase ==2:
+            if GAME_PHASE ==2:
                 sel_but = -1
                 while True:
                     sel_but = random.randint(0,len(butttons))
@@ -157,12 +187,12 @@ def buttons_work():
                 while True:
                     if sel_but == start_button_num:
                         if start_button.sensor.is_active:
-                            game_points += start_button.points_per_click
+                            GAME_POINTS += start_button.points_per_click
                             start_button.led.off()
                             break
                     else:       
                         if  butttons[sel_but].sensor.is_active:
-                            game_points += butttons[sel_but].points_per_click
+                            GAME_POINTS += butttons[sel_but].points_per_click
                             butttons[sel_but].led.off()
                             break
                     time_cnt += 1
@@ -171,8 +201,10 @@ def buttons_work():
                         butttons[sel_but].led.off()
                         break
 
-@app.route('/end_music', methods=['POST'])
+@APP.route('/end_music', methods=['POST'])
 def end_music():
+    """Метод проигрывает музыку об окончании игры
+    """
     play_music('static/music/end_game.mp3')
 
 if __name__ == '__main__':
@@ -182,5 +214,4 @@ if __name__ == '__main__':
     st_work.start()
     ob_work = Thread(target=buttons_work)
     ob_work.start()
-    app.run(host='127.0.0.1', port=8080, debug=True)
-    open_new('127.0.0.1')
+    APP.run(host='127.0.0.1', port=8080)
